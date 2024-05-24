e634a4fd677c46bfbdaeea0237c62f4588eebe78
Geo <151434365+georizki@users.noreply.github.com>
2024-05-24 08:01:10 +0700

    Update and rename SysRental.php to index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
             background-color: #f0f0f0;
             margin: 0;
             padding: 0;
-            
         }
         center {
             background-color: #ffffff;
@@ -26,7 +25,6 @@
             border-radius: 10px;
             box-shadow: black 3px 3px 3px;
         }
-
         h1 {
             color: white;
         }
@@ -34,13 +32,11 @@
             display: flex;
             flex-direction: column;
             align-items: center;
-            
         }
         label, select, input {
             text-align: center;
             margin: 10px 0;
             width: 100%;
-            
         }
         select, input[type="number"], input[type="text"] {
             padding: 10px;
@@ -68,15 +64,15 @@
 <body>
     <center>
         <div class="title">
-        <h1>Rental Motor</h1>
+            <h1>Rental Motor</h1>
         </div>
         <form action="" method="POST">
             <label>Nama Pelanggan</label>
             <input type="text" name="nama" id="nama" required><br>
-            <label>WaktuRental(Perhari) :</label>
-            <input type="number" name="Waktu" id="Waktu" placeholder="Masukan Lama Rental" required>   <br>       
+            <label>Waktu Rental (Perhari) :</label>
+            <input type="number" name="Waktu" id="Waktu" placeholder="Masukan Lama Rental" required><br>
             <label>Pilih Tipe Motor :</label>
-            <select name="JenisMotor" id="JenisMotor"> 
+            <select name="JenisMotor" id="JenisMotor" required>
                 <option disabled selected> --- Pilih Tipe Motor ---</option>
                 <option value="Kawasaki">Kawasaki</option>
                 <option value="Kago">Kago</option>
@@ -85,17 +81,17 @@
             </select><br>
             <input type="submit" value="kirim" name="kirim">
         </form>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            include 'Rental.php';
+            $Rental = new Rent();
+            $Rental->Setharga(70000, 140000, 210000, 280000);
+            $Rental->nama = $_POST['nama'];
+            $Rental->Waktu = $_POST['Waktu'];
+            $Rental->JenisMotor = $_POST['JenisMotor'];
+            $Rental->CetakPembelian();
+        }
+        ?>
     </center>
-</body> 
-<?php 
-    include 'Rental.php';
-    $Rental = new Rent();
-    $Rental ->Setharga(70000, 140000,210000,280000);
-    $Rental ->nama =$_POST['nama'];
-    $Rental ->Waktu =$_POST['Waktu'];
-    $Rental ->JenisMotor =$_POST['JenisMotor'];
-    echo $Rental->CetakPembelian();
-
-
-?>
+</body>
 </html>
